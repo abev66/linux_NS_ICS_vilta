7f9d513612d8119b912446b460fdb2313faa4009
Wayne Lai <abev66@gmail.com>
2012-07-17 19:00:46 +0800

    fsynccontrol: add a do_fsync counter
    Add a counter to do fsync while a certain do fsync action has been skipped.
    Try to balance between I/O performance and file system stablilty.

--- drivers/misc/fsync_control.c
+++ drivers/misc/fsync_control.c
@@ -14,9 +14,21 @@
 #define FSYNCCONTROL_VERSION 1
 
 static bool fsync_enabled = false;
+static unsigned int do_fsync_counter = 1000;
 
 bool fsynccontrol_fsync_enabled()
 {
+    static unsigned int skip_fsync_count = 0;
+  
+    if(!fsync_enabled && (do_fsync_counter != 0))
+      if(skip_fsync_count > do_fsync_counter) {
+	skip_fsync_count = 0;
+	pr_info("fsync_control: do fsync\n");
+	return true;
+      }
+      else
+	skip_fsync_count++;
+      
     return fsync_enabled;
 }
 EXPORT_SYMBOL(fsynccontrol_fsync_enabled);
@@ -63,13 +75,33 @@ static ssize_t fsynccontrol_version(struct device * dev, struct device_attribute
     return sprintf(buf, "%u\n", FSYNCCONTROL_VERSION);
 }
 
+static ssize_t do_fsync_counter_status_read(struct device * dev, struct device_attribute * attr, char * buf)
+{
+    return sprintf(buf, "%u\n", do_fsync_counter);
+}
+
+static ssize_t do_fsync_counter_status_write(struct device * dev, struct device_attribute * attr, const char * buf, size_t size)
+{
+    unsigned int data;
+    
+    if(sscanf(buf, "%u\n", &data) == 1) {
+	do_fsync_counter = data;
+	pr_info("%s: do_fsync_counter = %u\n", __FUNCTION__, do_fsync_counter);
+    } else
+	pr_info("%s: invalid input\n", __FUNCTION__);
+    
+    return size;
+}
+
 static DEVICE_ATTR(fsync_enabled, S_IRUGO | S_IWUGO, fsynccontrol_status_read, fsynccontrol_status_write);
 static DEVICE_ATTR(version, S_IRUGO , fsynccontrol_version, NULL);
+static DEVICE_ATTR(do_fsync_counter, S_IRUGO | S_IWUGO , do_fsync_counter_status_read, do_fsync_counter_status_write);
 
 static struct attribute *fsynccontrol_attributes[] = 
     {
 	&dev_attr_fsync_enabled.attr,
 	&dev_attr_version.attr,
+	&dev_attr_do_fsync_counter.attr,
 	NULL
     };
 
@@ -82,6 +114,12 @@ static struct miscdevice fsynccontrol_device =
     {
 	.minor = MISC_DYNAMIC_MINOR,
 	.name = "fsynccontrol",
+    };
+    
+static struct miscdevice do_fsync_counter_device = 
+    {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "do_fsync_counter",
     };
 
 static int __init fsynccontrol_init(void)
